1f1854feb6eae880d0caa85ed4fc0ebd3d57ee8b
theminecoder <dev@hawks008.dyndns.org>
2017-02-18 05:49:49 +1100

    Initial Commit

---
+++ src/main/java/me/theminecoder/bungeecord/bossbar/BarColor.java
@@ -0,0 +1,15 @@
+package me.theminecoder.bungeecord.bossbar;
+
+/**
+ * @author theminecoder
+ * @version 1.0
+ */
+public enum BarColor {
+    PINK,
+    BLUE,
+    RED,
+    GREEN,
+    YELLOW,
+    PURPLE,
+    WHITE
+}
